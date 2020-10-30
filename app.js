2da97ce269e201e6de9fa6f7ac3de2bea2fa2f80
hiba1725 <hmh79@mail.aub.edu>
2020-10-30 21:36:47 +0200

    first commit, side navbar and top search bar

---
+++ app.js
@@ -0,0 +1,28 @@
+
+window.onload = function() {
+    const openMenu = $('show-menu')
+    openMenu.addEventListener('click', toggleMenuShow);
+    
+    const hideMenuIcon = $('hide-menu')
+    hideMenuIcon.addEventListener('click', toggleMenuHide);
+};
+
+function $(id) {
+    return document.getElementById(id);
+}
+
+function toggleMenuShow() {
+    var sideMenu = $('nav-menu');
+    sideMenu.classList.add('active');
+    
+    var staticSideMenu = $('nav-left-static');
+    staticSideMenu.classList.add('nav-left-static-disappear');
+}
+
+function toggleMenuHide() {
+    var sideMenu = $('nav-menu');
+    sideMenu.classList.remove('active');
+    
+    var staticSideMenu = $('nav-left-static');
+    staticSideMenu.classList.remove('nav-left-static-disappear');
+}
