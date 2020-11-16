ee7fed12e759365aae5341235af6e80625b09358
AliMazloum <44380617+AliMazloum@users.noreply.github.com>
2020-11-16 20:06:04 +0200

    Update app.js
    AddVideo Function is add

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ window.onload = function() {
     
     const hideMenuIcon = $('hide-menu')
     hideMenuIcon.addEventListener('click', toggleMenuHide);
+    
+    const addVideo = $("add-video");
+    addVideo.addEventListener('click',AddVideo);
 };
 
 function $(id) {
@@ -34,3 +37,37 @@ function openNav() {
     document.getElementById("mySidenav").style.display="none"
     
   }
+
+function AddVideo(){
+    var mainBodyDiv = $("main-body");
+    var boxDiv = document.createElement("div");
+    boxDiv.classList.add("video-box-unit-main-page");
+    mainBodyDiv.appendChild(boxDiv);
+    var videoDiv = document.createElement("div");
+    videoDiv.style.background = "red";
+    videoDiv.style.height = "200px";
+    videoDiv.classList.add("video-diplayed-main-page");
+    boxDiv.appendChild(videoDiv);
+
+    var infoDiv = document.createElement("div");
+    infoDiv.classList.add("video-ifo");
+
+    var divName = document.createElement("div");
+    var divArtist = document.createElement("div");
+    var divViews = document.createElement("div");
+
+    divName.innerHTML = "Name: Ali Mazloum";
+    divArtist.innerHTML = "Name again: Ali Mazloum again";
+    divViews.innerHTML = "2.1M    views 3 years age";
+
+    divViews.id = "divViews";
+
+    infoDiv.appendChild(divName);
+    infoDiv.appendChild(document.createElement("br"));
+    infoDiv.appendChild(divArtist);
+    infoDiv.appendChild(document.createElement("br"));
+    infoDiv.appendChild(divViews);
+
+    boxDiv.appendChild(infoDiv);
+
+}
